f1b3e79b3d0884117af63a7a7a7b716e445cebde
ezedev <ezequiel.ivan.guzman@gmail.com>
2018-01-27 12:41:56 -0300

    juego

---
+++ Assets/Scripts/Personaje/Movimientos.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientos : MonoBehaviour {
+
+	public bool activado = true;
+	public System.Collections.Generic.List<string> señales = new List<string>();
+	private int indice;
+	// Use this for initialization
+	void Start () {
+		indice = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//si esta activado todavia se puede mandar mas señales
+		if(activado)
+		{
+			if( Input.GetKeyDown( KeyCode.Space ) )
+			{
+				señales.Add("space");
+				Debug.Log( "tocamos flecha espacio." );
+			}
+			if( Input.GetKeyDown( KeyCode.RightArrow ) )
+			{
+				señales.Add("right");
+				Debug.Log( "tocamos flecha derecha." );			 
+			}
+
+			if( Input.GetKeyDown( KeyCode.LeftArrow ) )
+			{
+				señales.Add("left");
+				Debug.Log( "tocamos flecha izquierda." );						
+			}
+			
+			if( Input.GetKeyDown( KeyCode.UpArrow ) )
+			{
+				señales.Add("up");
+				Debug.Log( "tocamos flecha arriba." );		
+			}
+
+			if( Input.GetKeyDown( KeyCode.DownArrow ) )
+			{
+				señales.Add("down");
+				Debug.Log( "tocamos flecha abajo." );
+							
+			}
+		}
+		
+           
+	}
+}
---
+++ Assets/Scripts/Personaje/Robot.cs
@@ -0,0 +1,129 @@
+﻿
+using UnityEngine;
+using System.Linq;
+
+public class Robot : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject movimiento;
+	private int indice = 0;
+    System.Collections.Generic.List<string> movs ;
+	private string[] señ;
+
+	private float timer = 1.0f;
+	private float timerReset= 1.0f;
+
+	private bool arriba= false;
+	private bool derecha = false;
+
+	private bool izquierda = false;
+
+	private bool abajo = false;
+
+	private int cantidadSeñales= 0;
+	void Start () {
+		movs = movimiento.GetComponent<Movimientos>().señales;
+		cantidadSeñales = movs.Count;
+		Debug.Log(cantidadSeñales);
+		señal(indice);
+	
+		/*foreach(string movimiento in movs)
+		{
+			if(movimiento.Equals("up"))
+			{	
+
+				//transform.Translate ( Vector2.up  *Time.deltaTime );
+			}
+			
+		}*/
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(arriba)
+		{
+			transform.Translate ( Vector2.up  *Time.deltaTime );
+			timer -= Time.deltaTime;
+			if(timer < 0)
+			{
+				arriba = false;
+				timer = timerReset;
+				señal(indice);
+			}
+			
+		}
+
+		if(derecha)
+		{
+			transform.Translate ( Vector2.right  *Time.deltaTime );
+			timer -= Time.deltaTime;
+			if(timer < 0)
+			{
+				derecha = false;
+				timer = timerReset;
+				señal(indice);
+			}
+		}
+		if(izquierda)
+		{
+			transform.Translate ( Vector2.left  *Time.deltaTime );
+			timer -= Time.deltaTime;
+			if(timer < 0)
+			{
+				izquierda = false;
+				timer = timerReset;
+				señal(indice);
+			}
+		}
+
+		if(abajo)
+		{
+			transform.Translate ( Vector2.down  *Time.deltaTime );
+			timer -= Time.deltaTime;
+			if(timer < 0)
+			{
+				abajo = false;
+				timer = timerReset;
+				señal(indice);
+			}
+		}
+
+		
+	}
+
+	public void señal(int numero)
+	{
+
+		if(movs.ElementAt(numero) == "up")
+		{
+			arriba= true;
+			indice++;
+		}
+		if(movs.ElementAt(numero) == "down")
+		{
+			abajo= true;
+			indice++;
+		}
+
+		if(movs.ElementAt(numero) == "right")
+		{
+			derecha= true;
+			indice++;
+			
+		}
+		if(movs.ElementAt(numero) == "left")
+		{
+			izquierda= true;
+			indice++;
+		}
+		if(movs.ElementAt(numero) == "space")
+		{
+			//derecha= true;
+			indice++;
+		}
+	//movs.ElementAt(numero);
+		//return movs.ElementAt(numero);;
+	}
+}
